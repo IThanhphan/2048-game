efbe63ee87917d9ff257d34c72431f48cf72412b
IThanh <thanhphanscpy2005@gmail.com>
2024-04-21 21:58:05 +0700

    update interface game

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 const BOARD_SIZE = 4;
 const BLOCK_SIZE = 150;
-const COLOR_MAPPING = [
-    'white',
-    'orange',
-]
+const COLOR_MAPPING = {
+    '0': 'rgb(205, 193, 180)',
+    '2': 'rgb(238, 228, 218)',
+    '4': 'rgb(238, 225, 201)',
+    '8': 'rgb(243, 178, 122)',
+    '16': 'rgb(246, 150, 100)',
+    '32': 'rgb(247, 124, 95)',
+    '64': 'rgb(247, 95, 59)',
+    '128': 'rgb(237, 208, 115)',
+    '256': 'rgb(237, 204, 98)',
+    '512': 'rgb(236, 199, 89)',
+    '1024': 'rgb(231, 195, 85)',
+    '2048': 'rgb(232, 190, 78)'
+}
 const WhiteID = 0;
-const FONT_SIZE = 100;
 
 const canvas = document.querySelector('#board');
 const ctx = canvas.getContext('2d');
 const playBtn = document.querySelector('.start-game button');
 const container = document.querySelector('.container');
 const scoreSpan = document.querySelector('.score span');
+const gameOver = document.querySelector('.game-over');
 
 container.setAttribute('style', `grid-template-columns: ${BLOCK_SIZE*BOARD_SIZE+20}px 200px`);
 
 let playing = 0;
 let score = 0;
+let FONT_SIZE = 100;
 
 class Board {
     constructor(ctx) {
@@ -28,16 +39,17 @@ class Board {
 
     reset() {
         score = 0;
+        gameOver.innerText = '';
         scoreSpan.innerText = score;
         this.grid = this.generateWhiteBoard();
-        this.drawBoard();
         for (let row=0; row<BOARD_SIZE; row++) {
             for (let col=0; col<BOARD_SIZE; col++) {
                 this.ctx.clearRect(row*BLOCK_SIZE+1, col*BLOCK_SIZE+1, BLOCK_SIZE-1, BLOCK_SIZE-1);
             }
         }
+        this.drawBoard();
         board.generateRandomNumber();
-        board.generateRandomNumber();
+        board.generateRandomNumber(); 
     }
 
     generateWhiteBoard() {
@@ -45,13 +57,21 @@ class Board {
     }
 
     drawCell(x, y, num) {
+        this.ctx.fillStyle = COLOR_MAPPING[`${num}`];
+        this.ctx.fillRect(x*BLOCK_SIZE, y*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
         if (num) {
+            FONT_SIZE = (num>100) ? 70 : 100;
             this.ctx.textAlign = 'center';
             this.ctx.font = `${FONT_SIZE}px Arial`;
-            this.ctx.fillStyle = 'black';
-            this.ctx.fillText(num, (x*BLOCK_SIZE)+(BLOCK_SIZE/2), (y*BLOCK_SIZE)+FONT_SIZE);  
+            if (num === 2 || num === 4) {
+                this.ctx.fillStyle = 'rgb(119, 110, 101)';
+            } else {
+                this.ctx.fillStyle = 'rgb(249, 246, 242)';
+            }
+            this.ctx.fillText(num, (x*BLOCK_SIZE)+(BLOCK_SIZE/2), (y*BLOCK_SIZE)+FONT_SIZE+15);  
         }
-        this.ctx.fillStyle = 'black';
+        this.ctx.strokeStyle = 'rgb(187, 173, 160)';
+        this.ctx.lineWidth = 10;
         this.ctx.strokeRect(x*BLOCK_SIZE, y*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
     }
     
@@ -240,6 +260,40 @@ class Board {
             scoreSpan.innerText = score;
         }
     }
+    isOver() {
+        for (let row=0; row<BOARD_SIZE; row++) {
+            for (let col=0; col<BOARD_SIZE; col++) {
+                if (col===BOARD_SIZE-1 && row===BOARD_SIZE-1) {
+                    return true;
+                }
+                if (col===BOARD_SIZE-1) {
+                    if (this.grid[row][col]===this.grid[row+1][col]) {
+                        return false;
+                    }
+                } else if (row===BOARD_SIZE-1) {
+                    if (this.grid[row][col]===this.grid[row][col+1]) {
+                        return false;
+                    }
+                } else {
+                    if (this.grid[row][col]===this.grid[row][col+1] || this.grid[row][col]===this.grid[row+1][col]) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    isWin() {
+        for (let row=0; row<BOARD_SIZE; row++) {
+            for (let col=0; col<BOARD_SIZE; col++) {
+                if (this.grid[row][col]===2048) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
 
 let board = new Board(ctx);
@@ -251,21 +305,33 @@ playBtn.addEventListener('click', function(e) {
 })
 
 document.addEventListener('keyup', function(e) {
-    switch (e.code) {
-        case 'ArrowDown':
-            board.moveDown();
-            break;
-        case 'ArrowUp':
-            board.moveUp();
-            break;
-        case 'ArrowRight':
-            board.moveRight();
-            break;
-        case 'ArrowLeft':
-            board.moveLeft();
-            break;
-        default:
-            break;
+    if (playing) {
+        switch (e.code) {
+            case 'ArrowDown':
+                board.moveDown();
+                break;
+            case 'ArrowUp':
+                board.moveUp();
+                break;
+            case 'ArrowRight':
+                board.moveRight();
+                break;
+            case 'ArrowLeft':
+                board.moveLeft();
+                break;
+            default:
+                break;
+        }
+    }
+    if (board.isWin()) {
+        playing = 0;
+        gameOver.innerText = 'You Win!!!'; 
+    }
+    if (board.isFill() && board.isOver()) {
+        playing = 0;
+        gameOver.innerText = 'Game Over!!!';
     }
 })
+
+
 
